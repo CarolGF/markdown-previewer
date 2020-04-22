75909256aa33e970b6a33e3bbfa89c01b458a38d
CarolGF <karola_gf@hotmail.com>
2020-04-22 16:01:06 +0100

    Updates

---
+++ precache-manifest.30d8915d801d6dba424262c757aa6156.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eeca56cd2f4914c921a82dfca3ab6a2",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "ba9abe310684675188fd",
+    "url": "/markdown-previewer/static/css/main.cc2f6284.chunk.css"
+  },
+  {
+    "revision": "311ffc9c0a6d90b7effb",
+    "url": "/markdown-previewer/static/js/2.4d651c6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/markdown-previewer/static/js/2.4d651c6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba9abe310684675188fd",
+    "url": "/markdown-previewer/static/js/main.f1a60308.chunk.js"
+  },
+  {
+    "revision": "fbddd20d8b4b59fb34c8",
+    "url": "/markdown-previewer/static/js/runtime-main.16601071.js"
+  }
+]);
